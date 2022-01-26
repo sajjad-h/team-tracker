bd09136f128a84d4d5b4f37d00aaab54a5aab07b
sajjad-h <hoosain.sajjad@gmail.com>
2022-01-26 16:18:27 +0600

    google sign-in and sign-out done

--- TeamTracker/app/src/main/java/com/example/teamtracker/MainActivity.java
+++ TeamTracker/app/src/main/java/com/example/teamtracker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.teamtracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,84 +18,82 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "main activity";
+    private static final String TAG = "main-activity-tag";
     private GoogleSignInClient mGoogleSignInClient = null;
-    final Integer RC_SIGN_IN = 100;
+    private final Integer RC_SIGN_IN = 100;
+    private SignInButton signInButton;
+    private Button signOutButton;
+    private TextView helloTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
-
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-        // Check for existing Google Sign In account, if the user is already signed in
-        // the GoogleSignInAccount will be non-null.
+        signInButton = findViewById(R.id.sign_in_button);
+        signOutButton = findViewById(R.id.sign_out_button);
+        helloTextView = (TextView) findViewById(R.id.helloText);
 
-
-        // Set the dimensions of the sign-in button.
-        SignInButton signInButton = findViewById(R.id.sign_in_button);
-        signInButton.setSize(SignInButton.SIZE_WIDE);
-
-        findViewById(R.id.hiButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "hello signing in", Toast.LENGTH_LONG).show();
-                System.out.println("you clicked Hi Button!");
-            }
-        });
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(MainActivity.this);
+        if (account == null) {
+            makeSignedOutButtonState();
+        }
+        else {
+            makeSignedInButtonState();
+        }
 
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "hello signing in", Toast.LENGTH_SHORT).show();
-                GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(MainActivity.this);
-                if (account == null) {
-                    signIn();
-                }
-                else {
-                    System.out.println("Already Signed In!");
-                    Toast.makeText(MainActivity.this, "Already Signed In!", Toast.LENGTH_SHORT).show();
-                }
+                signIn();
+            }
+        });
+
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
             }
         });
     }
 
+    private void makeSignedInButtonState() {
+        signInButton.setVisibility(View.GONE);
+        signOutButton.setVisibility(View.VISIBLE);
+    }
+
+    private void makeSignedOutButtonState() {
+        signInButton.setVisibility(View.VISIBLE);
+        signOutButton.setVisibility(View.GONE);
+    }
+
     private void signIn() {
-        Toast.makeText(this, "hello signing in", Toast.LENGTH_SHORT).show();
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
-        }
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        handleSignOutResult();
+                    }
+                });
     }
 
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
-            // Signed in successfully, show authenticated UI.
-            System.out.println("authenticated successful!");
             GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
             if (acct != null) {
                 String personName = acct.getDisplayName();
@@ -102,17 +102,28 @@ public class MainActivity extends AppCompatActivity {
                 String personEmail = acct.getEmail();
                 String personId = acct.getId();
                 Uri personPhoto = acct.getPhotoUrl();
-                TextView helloTextView = (TextView) findViewById(R.id.helloText);
                 helloTextView.setText("Hello " + personName);
+                makeSignedInButtonState();
+                Toast.makeText(this, "Successfully Signed In!", Toast.LENGTH_SHORT).show();
             }
 
         } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
             Log.d(TAG, "signInResult:failed code=" + e.getStatusCode());
-            System.out.println("Some Error Happened!");
         }
     }
 
+    private void handleSignOutResult() {
+        makeSignedOutButtonState();
+        Toast.makeText(this, "Successfully Signed Out!", Toast.LENGTH_SHORT).show();
+        helloTextView.setText("Hello World!");
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
 }
