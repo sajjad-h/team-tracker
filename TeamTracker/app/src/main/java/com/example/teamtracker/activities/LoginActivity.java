f435524ec2b125018df363844c8e2e4db660ae85
sajjad-h <59442218+sajjad-h@users.noreply.github.com>
2022-01-29 11:59:43 +0600

    Update LoginActivity.java

--- TeamTracker/app/src/main/java/com/example/teamtracker/activities/LoginActivity.java
+++ TeamTracker/app/src/main/java/com/example/teamtracker/activities/LoginActivity.java
@@ -14,7 +14,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
